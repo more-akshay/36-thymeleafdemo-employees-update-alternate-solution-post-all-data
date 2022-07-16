3acde54b2dfdc7933d267132b9e147c058e1e3cd
more-akshay <moreakshayb15@gmail.com>
2022-07-16 23:14:30 +0530

    thymeleaf-employees-update

---
+++ src/test/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.luv2code.springboot.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ThymeleafdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
